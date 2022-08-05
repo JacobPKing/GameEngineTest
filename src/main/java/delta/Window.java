dc63adfe0681fb2d80171460e82267401ea2131f
Jacob King <jking337@gatech.edu>
2022-08-04 22:39:36 -0400

    Initial Commit

---
+++ src/main/java/delta/Window.java
@@ -0,0 +1,104 @@
+package delta;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+/**
+ * @version 1.0
+ */
+public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    public static Window get() {
+        if(Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion());
+
+        try {
+            init();
+        } catch (IllegalStateException e) {
+            throw new RuntimeException(e);
+        }
+        loop();
+
+        //Free memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        //Terminate GLFW and free error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init() throws IllegalStateException {
+        //Set up error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //Init GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //Make window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW window");
+        }
+
+        //Init mouse listeners
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+
+        //Init Key Listener
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        //Make OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        //Enable vsync
+        glfwSwapInterval(1);
+
+        //Make window visible
+        glfwShowWindow(glfwWindow);
+
+        //Context
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            //Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
